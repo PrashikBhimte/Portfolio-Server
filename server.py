2c7023706336b15a162e16f6498e275835531052
PrashikBhimte <prashikbhimte29@gmail.com>
2025-03-26 18:24:11 +0530

    server is shifted from twilio messages to emails.

--- server.py
+++ server.py
@@ -2,17 +2,27 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from os import getenv
-from twilio.rest import Client
+# from twilio.rest import Client
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 load_dotenv()
 
-accountSID = getenv('TWILIO_ACCOUNT_SID')
-auccAuth = getenv('TWILIO_AUTH_TOKEN')
-number = getenv('TWILIO_PHONE_NO')
+# accountSID = getenv('TWILIO_ACCOUNT_SID')
+# auccAuth = getenv('TWILIO_AUTH_TOKEN')
+# number = getenv('TWILIO_PHONE_NO')
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = getenv("SENDER_EMAIL")
+SENDER_PASSWORD = getenv("SENDER_PASSWORD")  
+RECEIVER_EMAIL = getenv("RECEIVER_EMAIL")
 
 server = Flask(__name__)
 CORS(server)
-client = Client(accountSID, auccAuth)
+# client = Client(accountSID, auccAuth)
 
 @server.route('/', methods=['GET'])
 def default():
@@ -22,14 +32,27 @@ def default():
 def addData():
     data = dict(request.get_json())
     try : 
-        client.messages.create(
-        body= f"\nName : {data['name']}\nEmail : {data['email']}\nSubject : {data['subject']}\nMessage : {data['message']}",
-        from_= number,
-        to = "+918459058302"
-        )
-        return jsonify({ "status" : "good" }), 200
-    except :
-        return jsonify({ "status" : "bad" }), 200
+        body = data['message'] + "\n\n" + "From: " + data['name'] + "\n" + "Email: " + data['email']
+        message = MIMEMultipart()
+        message["From"] = SENDER_EMAIL
+        message["To"] = RECEIVER_EMAIL
+        message["Subject"] = data["subject"]
+        message.attach(MIMEText(body, "plain"))
+
+        try:
+            server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+            server.starttls()
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            
+            server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message.as_string())
+            
+            server.quit()
+            return jsonify({ "status" : "good" }), 200
+
+        except Exception as e:
+            return jsonify({ "status" : "bad"}), 500
+    except Exception as e:
+        return jsonify({ "status" : "bad" }), 500
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=8000)   
+    server.run(host='0.0.0.0', port=8000, debug=True)   

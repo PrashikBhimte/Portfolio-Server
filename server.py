4d9972440fe7e6431ebc729e65a82ecfbf56dd6b
PrashikBhimte <prashikbhimte29@gmail.com>
2025-03-26 18:39:42 +0530

    reday for production and requirements are updated.

--- server.py
+++ server.py
@@ -2,17 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from os import getenv
-# from twilio.rest import Client
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 load_dotenv()
-
-# accountSID = getenv('TWILIO_ACCOUNT_SID')
-# auccAuth = getenv('TWILIO_AUTH_TOKEN')
-# number = getenv('TWILIO_PHONE_NO')
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
@@ -22,7 +17,6 @@ RECEIVER_EMAIL = getenv("RECEIVER_EMAIL")
 
 server = Flask(__name__)
 CORS(server)
-# client = Client(accountSID, auccAuth)
 
 @server.route('/', methods=['GET'])
 def default():
@@ -55,4 +49,4 @@ def addData():
         return jsonify({ "status" : "bad" }), 500
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=8000, debug=True)   
+    server.run(host='0.0.0.0', port=8000)  
